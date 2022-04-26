84e789e7ffdc600f5ce6f3434991c1dc3094c345
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-04-26 09:14:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+import Pages from './Pages';
+
+const categoryUrl = 'https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories';
+const bookUrl = 'https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books?categoryId=1';
+
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const getData = async () => {
+    const response = await fetch(`${bookUrl}&page=${page}`);
+    console.log(page)
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    (async function () {
+      setPosts(await getData())
+    })()
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>b3k3n</h1>
+      <Pages page={page} setPage={setPage} />
+      {
+        posts.length ? (
+          posts.map(p => (
+            <h2 key={p.id}>{p.title}</h2>
+          ))
+        ) :
+        <h2>No posts</h2>
+      }
     </div>
   );
 }
+
 
 export default App;
